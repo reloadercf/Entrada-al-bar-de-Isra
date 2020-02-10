8f67ca52d9c4304b1f4eac3f02d7784109dbf7bf
reloadercf <elcfm1@gmail.com>
2020-02-10 08:51:20 -0600

    Modificacion de validacion

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let edad=prompt('Ya alcanzas el timbre?')
+let edad=prompt('Cual es tu edad?')
 let nombre=prompt('Cual es tu nombre?')
 let trabajadores={nombreTrabajor:'Jose Luis',
                     Direccion:'En su casa',
@@ -19,10 +19,14 @@ console.log(edad)
 console.log(nombre)
 console.log(edad+nombre)
 console.log(trabajadores)
-if(edad>18){
-    document.getElementById('imprimir').innerHTML=`Hola ${nombre} tu edad ${edad} tienes una oferta de 2 chelas al precio de 1, el Gerente es: ${trabajadores.nombreTrabajor} `
 
+if(edad>0||edad<0||edad==0){
+    if(edad<100 && edad>17){
+        document.getElementById('imprimir').innerHTML=`Hola ${nombre} tu edad ${edad} tienes una oferta de 2 chelas al precio de 1, el Gerente es: ${trabajadores.nombreTrabajor} `
+    }else{
+        document.getElementById('imprimir').innerHTML=`Corrije tu edad  `
+    }
 }
 else{
-    document.getElementById('imprimir').innerHTML=`${nombre} no puedes entrar`
+    document.getElementById('imprimir').innerHTML=`${nombre} La edad tiene que ser numerica`
 }
